407d1acc19e9b02c1759c073fe4c5393816462e1
Samaa Gazzaz <sgazzaz@ucsc.edu>
2021-03-22 14:58:29 -0700

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -1,3 +1,5 @@
+# https://www.psycopg.org/docs/usage.html#passing-parameters-to-sql-queries
+
 # importing the library to connect with postgres
 import psycopg2
 
@@ -9,6 +11,9 @@ cur = conn.cursor()
 
 # this line executes the stored procedure 'storedProName()' 
 cur.execute("CALL schema.storedProName();")
+
+# this line executes a select query that returns all the records from table 'tableName' in the schema 'schema'
+cur.execute("SELECT * FROM schema.tableName;")
 
 # if the executed query/stored procedure returns records, those can be acquired like this
 records = cur.fetchall() 
